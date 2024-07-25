d5f4b9aac1422449147275ed60ab92e2f44c68e1
alynnesantos <alynnebianca774@gmail.com>
2024-07-25 12:51:15 -0300

    Ligação de dados bidirecional (diretiva v model)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <h1 :class="classVar">
-      Curso vue 3
-    </h1>
-      <!-- or -->
-    <h1 :class="{ 'title': true, 'title-home': isHome}">
-      Curso VUE 3
-    </h1>
-
-    <p :class="pClass">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde assumenda accusamus commodi, quia corrupti aut id facilis? Repudiandae temporibus odio ipsum non obcaecati corrupti cumque reiciendis eaque minima quaerat.
-    </p>
-
-    <p :style="{'color': 'aqua', backgroundColor: 'black'}">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate expedita iure sequi vitae. Suscipit quidem optio commodi consectetur, cupiditate nisi. Minima molestiae similique veritatis, eveniet amet nam esse rerum at.
-    </p>
-
     <!-- V-if -->
     <TheHeader v-if="showHeader" />
 
@@ -38,6 +22,79 @@
     </div>
 
     <!-- v-bind:src or :src -->
+
+    <br>
+    <hr>
+    <h1 :class="classVar">
+      Curso vue 3
+    </h1>
+    <!-- or -->
+    <h1 :class="{ 'title': true, 'title-home': isHome }">
+      Curso VUE 3
+    </h1>
+
+    <p :class="pClass">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde assumenda accusamus commodi, quia corrupti
+      aut id facilis? Repudiandae temporibus odio ipsum non obcaecati corrupti cumque reiciendis eaque minima quaerat.
+    </p>
+
+    <p :style="{ 'color': 'aqua', backgroundColor: 'black' }">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate expedita iure sequi vitae. Suscipit quidem
+      optio commodi consectetur, cupiditate nisi. Minima molestiae similique veritatis, eveniet amet nam esse rerum at.
+    </p>
+    <br>
+    <hr>
+
+    <div>Two-way data binding - v-model -> formulários</div>
+
+    <br>
+    <div>
+      <label>Name</label> <br>
+      <input v-model="name" type="text">
+      <br>
+      {{ name }}
+    </div>
+
+    <br>
+
+    <div>
+      <label>Sports</label> <br>
+      <select v-model="sports">
+        <option value="">Escolha</option>
+        <option value="futebol">Futebol</option>
+        <option value="skate">Skate</option>
+        <option value="volei">Vôlei</option>
+      </select> <br> {{ sports }}
+    </div>
+
+    <br>
+
+    <div>
+      <label>Newsletter</label><br>
+      <input v-model="newsletter" type="radio" value="Sim"> Sim
+      <input v-model="newsletter" type="radio" value="Não"> Não
+      <br> {{ newsletter }}
+    </div>
+
+    <br>
+
+    <div>
+      <label>Contract</label> <br>
+      <input v-model="contract" type="checkbox">
+      Aceita nossos termos...
+      <br> {{ contract }}
+    </div>
+
+    <br>
+
+    <div>
+      <label>Mark the colors you like most</label> <br>
+      <input v-model="colors" value="azul" type="checkbox">
+      Azul
+      <input v-model="colors" value="amarelo" type="checkbox">
+      Amarelo
+      <br> {{ colors }}
+    </div>
   </div>
 </template>
 
@@ -93,6 +150,11 @@ export default {
       classVar: 'title',
       isHome: true,
       pClass: ['text', 'text-home'],
+      name: '',
+      sports: '',
+      newsletter: '',
+      contract: '',
+      colors: []
     }
   }
 }
@@ -127,12 +189,13 @@ export default {
 .text-home {
   color: #33ff4e;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: start;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>

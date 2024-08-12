9b958142f6bfe3a9280e62a57ffe0269da797b36
alynnesantos <alynnebianca774@gmail.com>
2024-08-12 10:20:07 -0300

    Introdução ao Vue Router parte 1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,86 +1,41 @@
 <template>
-  <div>
-    <h1 class="card">Vue 3</h1>
-    <BaseAlert v-if="showAlert" :variant="variant" @close="onClose()">
-      {{ text }}
-    </BaseAlert>
-    <!-- <PAI> -> De cima pra baixo -> PROPS
-      <FILHO>
-        <NETO>
-          -> Debaixo pra cima -> emit-event
-        </NETO>
-      </FILHO>
-    </PAI> -->
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link :to="dynamicRoute">Services</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
 <script>
-import BaseAlert from '@/components/BaseAlert.vue';
-export default {
-  name: 'App',
-  components: {
-    BaseAlert
-  },
-
-  data() {
-    return {
-      variant: 'success',
-      text: 'Seu formulário foi enviado com sucesso!',
-      showAlert: 'true'
+  export default {
+    data() {
+      return {
+        dynamicRoute: {name: 'services'}
+      }
     }
-  },
-
-  methods: {
-    onClose() {
-      this.showAlert = false
-      console.log('on close');
-    },
-  },
-
-  computed: {
-  },
-
-  watch: {
-  },
-}
+  }
 </script>
 
 <style>
-.title {
-  color: blue;
-  font-size: 30px;
-}
-
-.title-home {
-  color: red;
-  font-size: 30px;
-}
-
-.text {
-  background-color: rgb(0, 0, 0);
-}
-
-.todos-item {
-  background: #10791e;
-  color: #fff;
-  border: 7px solid #cccccc7e;
-  padding: 10px;
-  margin-bottom: 10px;
-  margin-top: 15px;
-  display: flex;
-
-}
-
-.text-home {
-  color: #33ff4e;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: start;
+  text-align: center;
   color: #2c3e50;
-  margin-top: 30px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ app/src/views/ServicesView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="services">
+        <h1>Services</h1>
+        <p>This is a services page</p>
+    </div>
+</template>
